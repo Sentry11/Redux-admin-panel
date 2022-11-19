23ce8c2986c442639899b32177c23990cf28e53a
Sentry11 <99908368+Sentry11@users.noreply.github.com>
2022-11-19 02:06:20 +0200

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,9 @@
 import HeroesList from '../heroesList/HeroesList';
 import HeroesAddForm from '../heroesAddForm/HeroesAddForm';
 import HeroesFilters from '../heroesFilters/HeroesFilters';
-
 import './app.scss';
 
 const App = () => {
-    
     return (
         <main className="app">
             <div className="content">
@@ -18,5 +16,4 @@ const App = () => {
         </main>
     )
 }
-
 export default App;
